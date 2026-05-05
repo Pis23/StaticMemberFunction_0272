69406b1f53195397379aeffd1875d31087016c31
Pis23 <nafisshl10@gmail.com>
2026-05-05 19:08:21 +0700

    kelas mhs

--- ConstructorOverloading.cpp
+++ ConstructorOverloading.cpp
@@ -1,3 +1,16 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class mahasiswa {
+    private:
+    int nim;
+    string nama ;
+    public:
+    mahasiswa(); 
+    mahasiswa(int);
+    mahasiswa(string);
+    mahasiswa(int inim, string inama);
+    void cetak();
+    
+};
