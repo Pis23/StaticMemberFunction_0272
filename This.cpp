f639d6c36b47f3996cb37785acf7f60b826f105a
Pis23 <nafisshl10@gmail.com>
2026-05-05 19:07:25 +0700

    main

--- This.cpp
+++ This.cpp
@@ -10,3 +10,10 @@ class buku{
             return this->judul; // return variabel judul.
         }
 };
+
+int main()
+{
+    buku bukunya;
+    cout<<bukunya.setget("matematika");
+    return 0;
+}
